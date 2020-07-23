cc0d82f17ae7ad653ce9a29a1437405c11cd40ed
LGgameLAB <lukegonsalves07@gmail.com>
2020-07-23 07:58:17 -0700

    Removed outdated test exe build

--- exe build/dist/RPG Py Engine v2.5/rpg_village_tileset/readme.txt
+++
@@ -1,5 +0,0 @@
-Thank you for downloading this asset!
-
-If you found this useful please consider supporting me by buying my other assets ^-^
-
-https://frano-px.itch.io
--- exe build/dist/RPG Py Engine v2.5/tf_jungle/Jungle.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="Jungle" tilewidth="16" tileheight="16" tilecount="462" columns="22">
- <image source="tf_jungle/tf_jungle_tileset.png" width="352" height="336"/>
-</tileset>
